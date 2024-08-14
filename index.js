01cf9a519c54bb47232b9959d6a7c96f8c317671
ahmedalsanadi <ahmed11alsanadi11@gmail.com>
2024-08-14 14:18:26 +0300

    Completed

---
+++ index.js
@@ -0,0 +1,9 @@
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+const add = (x, y) => x + y;
+const square = (x) => x * x;
+
+const findSquareRoot = (x) => Math.sqrt(x);
+const isEven = (num) => num % 2 === 0;
+const calculateCircleArea = (radius_value) =>
+	Math.PI * radius_value * radius_value;
